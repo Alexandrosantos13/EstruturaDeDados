586397a6666022449aae2c3d805aa950c283ef75
Alexandrosantos13 <Alexandro.santos1305@gmail.com>
2025-09-19 11:24:22 -0300

    .

--- exercicios/Questao4.java
+++ exercicios/Questao4.java
@@ -8,10 +8,10 @@ public class Questao4 {
         palavra[0] = sc.nextLine();
         char [] aux = palavra[0].toLowerCase().toCharArray();
 
-        for (int i=aux.length; i>0; i--){
-            String letra=String.valueOf(aux[i-1]);
+        for (int i=0; i<aux.length; i++){
+            String letra=String.valueOf(aux[i]);
             if (!letra.equals("a") && !letra.equals("e") && !letra.equals("i") && !letra.equals("o") && !letra.equals("u")){
-                if (Character.isLetter(aux[i-1])) { 
+                if (Character.isLetter(aux[i])) { 
                     consoantes++;
                     System.out.println(letra);
                 }
@@ -20,7 +20,4 @@ public class Questao4 {
         System.out.println("O total de consoantes foi "+ consoantes);
     }
 }
-/*4. Faça um programa em Java que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.
- * quebre a palavra em 10 caracteres e atribua cada caractere a um slot em um array
- * dps faça um loop onde um contador vai receber ele mesmo mais 1 a cada não vogal
- */
+/*4. Faça um programa em Java que leia um vetor de 10 caracteres, e diga quantas consoantes foram lidas. Imprima as consoantes.*/
