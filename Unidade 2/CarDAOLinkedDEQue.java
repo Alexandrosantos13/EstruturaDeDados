1e3397ef83b69d03a9af074c78acf8b1d557a233
Alexandrosantos13 <Alexandro.santos1305@gmail.com>
2025-11-12 14:47:13 -0300

    implementada a operação getCarsByColor(String color) na classe CarDAOLinkedDEQue para retornar carros pela cor, utilizando uma fila temporária para preservar a ordem original.

--- Unidade 2/CarDAOLinkedDEQue.java
+++ Unidade 2/CarDAOLinkedDEQue.java
@@ -124,7 +124,19 @@ public class CarDAOLinkedDEQue implements CarDAO {
 
     @Override
     public Car[] getCarsByColor(String color) {
-        throw new UnsupportedOperationException("Operação ainda não implementada");
+        DEQueable<Car> tempdeque = new LinkedDEQue<>();
+        DEQueable<Car> result = new LinkedDEQue<>();
+        while (!cars.isEmpty()) {
+            Car temp = cars.dequeue();
+            tempdeque.enqueue(temp);
+            if (temp != null && temp.getColor() != null && temp.getColor().equalsIgnoreCase(color)) {
+                result.enqueue(temp);
+            }
+        }
+        while (!tempdeque.isEmpty()){
+            cars.enqueue(tempdeque.dequeue());
+        }
+        return queueToArray(result);
     }
 
     @Override
